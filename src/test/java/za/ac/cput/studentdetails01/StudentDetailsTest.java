e366afb1961e2b6060cdaa2e3caca8c15bdcd406
Zubair Esau- 217100554 <user@user-PC>
2021-04-01 07:50:50 +0200

    Testing all the methods of code

--- src/test/java/za/ac/cput/studentdetails01/StudentDetailsTest.java
+++ src/test/java/za/ac/cput/studentdetails01/StudentDetailsTest.java
@@ -7,12 +7,97 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Timeout;
 
 /**
  *
  * @author Breyton Ernstzen
  */
 public class StudentDetailsTest {
+    
+    @Disabled("Disable all test as given by intruction")
+     
+
+     @Test
+     @Timeout (5)
+    public void testEquility() throws InterruptedException{
+        StudentDetails student = new  StudentDetails();
+        StudentDetails majorSubject = new  StudentDetails();
+        StudentDetails minorSubject = new  StudentDetails();
+        StudentDetails calcAverage = new  StudentDetails();
+        
+        String expectedname = student.getStudentName();
+        String actualname = "John";
+        assertEquals(expectedname,actualname);
+        
+        String expectedMajor = majorSubject.getMajorsubject();
+        String actualMajor = "Maths";
+        assertEquals(expectedMajor,actualMajor);
+        
+        String expectedMinor = minorSubject.getMinorsubject();
+        String actualMinor = "Linux";
+        assertEquals(expectedMinor,actualMinor);
+        
+        double expectedAverage = calcAverage.getAverageamount();
+        String actualAverage = "60";
+        assertEquals(expectedAverage,actualAverage);
+         }
+    
+    
+     @Test
+     @Timeout (5)
+    public void testIdentity () throws InterruptedException{
+       StudentDetails  student = new StudentDetails ();
+       StudentDetails  majorSubject = new StudentDetails ();
+       StudentDetails  minorSubject = new StudentDetails ();
+       StudentDetails  calcAverage = new StudentDetails (); StudentDetails calc2Average = calcAverage;
+        
+        assertSame("John",student.getStudentName());
+        assertNotSame("Jade",student.getStudentName());
+        
+        assertSame("Maths",majorSubject.getMajorsubject());
+        assertNotSame("Projects",student.getStudentName());
+        
+        assertSame("Linux",minorSubject.getMinorsubject());
+       
+        
+        assertSame(calcAverage,calc2Average);
+        assertNotSame(student,majorSubject);
+        
+    }
+    
+     @Test
+     @Timeout (5)
+    public void testFailure () throws InterruptedException{
+          StudentDetails majorSubject = new  StudentDetails();
+          StudentDetails minorSubject = new  StudentDetails();
+         
+        String expectedMajor = majorSubject.getMajorsubject();
+        String actualMajor = "Afrikaans";
+        assertEquals(expectedMajor,actualMajor);
+        
+        String expectedMinor = minorSubject.getMinorsubject();
+        String actualMinor = "Projects";
+        assertEquals(expectedMinor,actualMinor);
+        
+    }
+    
+    @Test
+     @Timeout (5)
+    public void testTimeout() throws InterruptedException{
+        Thread.sleep(4000);
+        System.out.println("Test passed with good time");
+    }
+
+    
+    
+    
+    
+    
+    
+    
+    
     
     public StudentDetailsTest() {
     }
