057cf33b7f8dc12d9bafabad08e66f4db44e7ef9
Zubair Esau- 217100554 <>
2021-04-01 07:35:46 +0200

    Implementing the code that should be tested

--- src/main/java/za/ac/cput/studentdetails01/StudentDetails.java
+++ src/main/java/za/ac/cput/studentdetails01/StudentDetails.java
@@ -10,5 +10,39 @@ package za.ac.cput.studentdetails01;
  * @author Breyton Ernstzen - 217203027 (Group Lead)
  */
 public class StudentDetails {
-    
+        private String StudentName,Majorsubject,Minorsubject;
+    private double Averageamount;
+
+    public String getStudentName() {
+        return "John";
+    }
+   
+    public String getMajorsubject() {
+        return "Maths";
+    }
+
+    public void setMajorsubject(String Majorsubject) {
+        this.Majorsubject = Majorsubject;
+    }
+
+    public String getMinorsubject() {
+        return "Linux";
+    }
+
+    public void setMinorsubject(String Minorsubject) {
+        this.Minorsubject = Minorsubject;
+    }
+
+    public double getAverageamount() {
+        return 60;
+    }
+
+    public void setAverageamount(double Averageamount) {
+        this.Averageamount = Averageamount;
+    }
+
+    @Override
+    public String toString() {
+        return "Report{" + "studentName=" + StudentName + ", Majorsubject=" + Majorsubject + ", Minorsubject=" + Minorsubject + ", Averageamount=" + Averageamount + '}';
+    }
 }
